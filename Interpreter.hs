22cdccf1d61d0019172e000d40c8e783d237ceb4
Kotolegokot <gkotolegokot@gmail.com>
2016-12-08 16:11:55 +0300

    Add arithmetics operations

--- Interpreter.hs
+++ Interpreter.hs
@@ -13,13 +13,6 @@ interprete :: Tree Terminal -> Program
 interprete (Node (TKeyword "program") body) = Program Map.empty $ head body
 interprete _ = error "no 'program' at the beginning of the outer list"
 
-{--
-eval_function :: Map.Map String Function -> Tree Terminal -> IO Terminal
-eval_function functions (Node head args)
-  | isKeyword head = call_function functions (fromKeyword head) =<< mapM (eval_function functions) args
-  | otherwise      = if null args then return head else error "too many arguments"
---}
-
 eval_function :: Map.Map String Function -> Tree Terminal -> IO Terminal
 eval_function functions (Node head args)
   | isKeyword head = call_function functions (fromKeyword head) args
@@ -61,12 +54,8 @@ call_function functions "unless" args
              then eval_function functions arg3
              else eval_function functions arg2
 
---call_function functions "+" args
---    | length args <= 1 =  error "'+' requires more than two arguments"
---    | otherwise = args >>= eval_function functions >>= 
-
 call_function functions "=" args
-    | length args /= 2 = error "'=' requires two argumens"
+    | length args /= 2 = error "'=' requires two arguments"
     | otherwise = handle_eq args
     where handle_eq [arg1, arg2] = do
               exp1 <- eval_function functions arg1
@@ -74,7 +63,7 @@ call_function functions "=" args
               return . boolToTerminal $ exp1 == exp2
 
 call_function functions "/=" args
-    | length args /= 2 = error "'/=' requires two argumens"
+    | length args /= 2 = error "'/=' requires two arguments"
     | otherwise = handle_eq args
     where handle_eq [arg1, arg2] = do
               exp1 <- eval_function functions arg1
@@ -85,18 +74,65 @@ call_function functions "seq" args = handle_seq args
     where handle_seq [x]    = eval_function functions x
           handle_seq (x:xs) = eval_function functions x >> handle_seq xs
           handle_seq []     = return TNil
+
+call_function functions "+" args = do
+    (exps, return_type) <- num_args functions args
+
+    return $ case return_type of
+               ARInt   -> TInt . sum . fmap fromInt $ exps
+               ARFloat -> TFloat . sum . fmap fromNumber $ exps
+
+call_function functions "-" args
+  | length args /= 2 = error "'-' requires two arguments"
+  | otherwise        = do
+      ([x, y], return_type) <- num_args functions args
+      
+      return $ case return_type of
+                 ARInt   -> TInt   $ fromInt x - fromInt y
+                 ARFloat -> TFloat $ fromNumber x - fromNumber y
+
+call_function functions "*" args = do
+    (exps, return_type) <- num_args functions args
+
+    return $ case return_type of
+               ARInt   -> TInt . product . fmap fromInt $ exps
+               ARFloat -> TFloat . product . fmap fromNumber $ exps
+
+call_function functions "/" args
+  | length args /= 2 = error "'/' requires two arguments"
+  | otherwise        = do
+      ([x, y], return_type) <- num_args functions args
+
+      return $ case return_type of
+                 ARInt   -> TInt   $ fromInt x `div` fromInt y
+                 ARFloat -> TFloat $ fromNumber x / fromNumber y
               
 call_function _ func _ = error $ "undefined function '" ++ func ++ "'"
+
+data ArithmReturn = ARInt | ARFloat
+num_args :: Map.Map String Function -> Forest Terminal -> IO ([Terminal], ArithmReturn)
+num_args functions args = helper args [] ARInt
+    where helper (x:xs) exps ARInt = do
+            exp <- eval_function functions x
+        
+            case exp of
+              TInt   _ -> helper xs (exps ++ [exp]) ARInt
+              TFloat _ -> helper xs (exps ++ [exp]) ARFloat
+              _        -> error "float or int expected"
+
+          helper (x:xs) exps ARFloat = do
+            exp <- eval_function functions x
+
+            case exp of
+              TInt   _ -> helper xs (exps ++ [exp]) ARFloat
+              TFloat _ -> helper xs (exps ++ [exp]) ARFloat
+              _        -> error "float or int expected"
+
+          helper [] exps return_type = return (exps, return_type)
 
 {--
 -- built-in functions
 call_function :: Map.Map String Function -> String -> [Terminal] -> IO Terminal
-call_function _ "+"        args = add_ args
-call_function _ "-"        args = substract_ args
-call_function _ "*"        args = product_ args
-call_function _ "/"        args = divide_ args
-call_function _ "="        args = equal_ args
-call_function _ "/="       args = inequal_ args
 call_function _ "<"        args = lt_ args
 call_function _ ">"        args = gt_ args
 call_function _ "<="       args = le_ args
@@ -106,5 +142,4 @@ call_function _ "|"        args = or_ args
 call_function _ "->"       args = impl_ args
 call_function _ "not"      args = not_ args
 call_function _ "float"    args = float_ args
-call_function _ func       _    = error $ "undefined function '" ++ func ++ "'"
 --}
