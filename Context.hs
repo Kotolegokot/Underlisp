be99196acd3ed79ed76ae9b015fa3cee6e089788
Kotolegokot <gkotolegokot@gmail.com>
2016-12-08 19:38:08 +0300

    Change Context

--- Context.hs
+++ Context.hs
@@ -1,9 +1,7 @@
-module Context (Function(..), Context) where
+module Context (Context) where
 
 import qualified Data.Map as Map
 import Data.Tree
 import SemanticAnalyzer
 
-data Function = Function [String] (Tree Terminal)
-
-type Context = Map.Map String Function
+type Context = Map.Map String Terminal
