d2f4088202eb9386bbd707e35c10b524a7c8c281
Kotolegokot <gkotolegokot@gmail.com>
2016-12-08 11:50:20 +0300

    Add some arithmetic functions

--- Interpreter.hs
+++ Interpreter.hs
@@ -6,19 +6,62 @@ import qualified Data.Map as Map
 import Program
 import SemanticAnalyzer
 
+evaluate :: Program -> IO ()
+evaluate program@(Program functions body) = void $ eval_function functions body
+
 interprete :: Tree Terminal -> Program
 interprete (Node (TKeyword "program") body) = Program Map.empty $ head body
 interprete _ = error "no 'program' at the beginning of the outer list"
-
-evaluate :: Program -> IO ()
-evaluate program@(Program functions body) = void $ eval_function functions body
 
 eval_function :: Map.Map String Function -> Tree Terminal -> IO Terminal
 eval_function functions (Node head args)
   | isKeyword head = call_function functions (fromKeyword head) =<< mapM (eval_function functions) args
   | otherwise      = if null args then return head else error "too many arguments"
 
+-- built-in functions
 call_function :: Map.Map String Function -> String -> [Terminal] -> IO Terminal
-call_function functions "print" args
+
+-- print function
+call_function _ "print" args
   | length args /= 1 = error "'print' expected only one argument"
-  | otherwise        = print (head args) >> return TNil
+  | otherwise        = (putStr . printTerminal) (head args) >> return TNil
+
+-- plus function
+call_function _ "+" args = return $ case check_num_args args of
+                                      ARFloat -> TFloat . sum $ fmap fromNumber args
+                                      ARInt   -> TInt . sum $ fmap fromInt args
+
+-- minus function
+call_function _ "-" args@(x:y:[]) = return $ case check_num_args args of
+                                               ARFloat -> TFloat $ fromNumber x - fromNumber y
+                                               ARInt   -> TInt $ fromInt x - fromInt y
+call_function _ "-" _ = error "'-' expected two arguments"
+
+-- product function
+call_function _ "*" args = return $ case check_num_args args of
+                                      ARFloat -> TFloat . product $ fmap fromNumber args
+                                      ARInt   -> TInt . product $ fmap fromInt args
+
+-- divide function
+call_function _ "/" args@(x:y:[]) = return $ case check_num_args args of
+                                              ARFloat -> TFloat $ fromNumber x / fromNumber y
+                                              ARInt   -> TInt $ fromInt x `div` fromInt y
+call_function _ "/" _ = error "'/' expected two arguments"
+
+
+call_function _ _ _ = error "undefined function"
+
+data ArithmReturn = ARInt | ARFloat
+check_num_args :: [Terminal] -> ArithmReturn
+check_num_args args = helper args ARInt
+    where helper (x:xs) ARInt = case x of
+                                  TInt _   -> helper xs ARInt
+                                  TFloat _ -> helper xs ARFloat
+                                  _        -> error "float or int expected"
+
+          helper (x:xs) ARFloat = case x of
+                                   TInt _   -> helper xs ARFloat
+                                   TFloat _ -> helper xs ARFloat
+                                   _        -> error "float or int expected"
+
+          helper [] return_type = return_type
--- SemanticAnalyzer.hs
+++ SemanticAnalyzer.hs
@@ -2,29 +2,55 @@ module SemanticAnalyzer
     (Terminal(..),
     analyze,
     isKeyword,
-    fromKeyword) where
+    fromKeyword,
+    fromNumber,
+    fromFloat,
+    fromInt,
+    printTerminal) where
 
 import Text.Read
 import Data.Maybe
 import Data.Tree
 
-data Terminal = TInteger Int | TFloat Float | TString String | TChar Char | TT | TNil | TKeyword String
+data Terminal = TInt Int | TFloat Float | TString String | TChar Char | TT | TNil | TKeyword String
   deriving (Eq, Show)
 
+printTerminal :: Terminal -> String
+printTerminal (TInt int)         = show int
+printTerminal (TFloat float)     = show float
+printTerminal (TString string)   = string
+printTerminal (TChar char)       = [char]
+printTerminal TT                 = "T"
+printTerminal TNil               = "TNil"
+printTerminal (TKeyword keyword) = keyword
+
+isKeyword :: Terminal -> Bool
 isKeyword (TKeyword _) = True
 isKeyword _            = False
 
+fromKeyword :: Terminal -> String
 fromKeyword (TKeyword a) = a
 fromKeyword _            = error "that's no keyword"
 
-data State = None | SInteger | SFloat | SString | SChar
+fromNumber :: Terminal -> Float
+fromNumber (TFloat f)     = f
+fromNumber (TInt i)       = fromIntegral i
+fromNumber _              = error "float or int expected"
+
+fromFloat :: Terminal -> Float
+fromFloat (TFloat f) = f
+fromFloat _          = error "float expected"
+
+fromInt :: Terminal -> Int
+fromInt (TInt i) = i
+fromInt _        = error "int expected"
 
 analyze :: Tree String -> Tree Terminal
 analyze = fmap analyze_terminal
     where analyze_terminal "T" = TT
           analyze_terminal "Nil" = TNil
           analyze_terminal terminal
-            | isJust tryInt    = TInteger $ fromJust tryInt
+            | isJust tryInt    = TInt     $ fromJust tryInt
             | isJust tryFloat  = TFloat   $ fromJust tryFloat
             | isJust tryChar   = TChar    $ fromJust tryChar
             | isJust tryString = TString  $ fromJust tryString
