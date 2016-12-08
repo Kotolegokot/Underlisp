85732feb0b4d82711c6087b7124316e66a57a727
XnekrosX <xxxnekrosxxx@gmail.com>
2016-12-08 15:00:06 +0300

    Add a bunch of functions

--- Interpreter.hs
+++ Interpreter.hs
@@ -89,6 +89,30 @@ call_function functions ">" args
               exp2 <- eval_function functions arg2
               return . boolToTerminal $ exp1 > exp2
 
+call_function functions "<" args
+    | length args /= 2 = error "'<' requires two argumens"
+    | otherwise = handle_gt args
+    where handle_gt [arg1, arg2] = do
+              exp1 <- eval_function functions arg1
+              exp2 <- eval_function functions arg2
+              return . boolToTerminal $ exp1 < exp2
+              
+call_function functions ">=" args
+    | length args /= 2 = error "'>=' requires two argumens"
+    | otherwise = handle_gt args
+    where handle_gt [arg1, arg2] = do
+              exp1 <- eval_function functions arg1
+              exp2 <- eval_function functions arg2
+              return . boolToTerminal $ exp1 >= exp2
+              
+call_function functions "<=" args
+    | length args /= 2 = error "'<=' requires two argumens"
+    | otherwise = handle_gt args
+    where handle_gt [arg1, arg2] = do
+              exp1 <- eval_function functions arg1
+              exp2 <- eval_function functions arg2
+              return . boolToTerminal $ exp1 <= exp2
+
 call_function functions "seq" args = handle_seq args
     where handle_seq [x]    = eval_function functions x
           handle_seq (x:xs) = eval_function functions x >> handle_seq xs
