65dea5a57f07ff8a4fd22e9be064d0bd9a675a5d
Kotolegokot <gkotolegokot@gmail.com>
2016-12-09 21:49:11 +0300

    Fix eval_sexpr

--- Evaluator.hs
+++ Evaluator.hs
@@ -13,10 +13,12 @@ evaluate _                                 = error "a program must start with ca
 
 eval_sexpr :: Context.Context -> SExpr -> IO SExpr
 eval_sexpr context (SList (SKeyword fname:body)) = call_function fname context body
-eval_sexpr _       (SList _) = error "keyword expected"
+eval_sexpr _       (SList _)                     = error "keyword expected"
 eval_sexpr context (SKeyword kword)
   | kword `Map.member` context = return $ context Map.! kword
   | otherwise                  = error $ "undefined indentificator '" ++ kword ++ "'"
+
+eval_sexpr _        sexpr                         = return sexpr
 
 call_function :: String -> Context.Context -> [SExpr] -> IO SExpr
 call_function fname = case fname of
