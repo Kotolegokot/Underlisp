c2618d5739cb866c45e789cb35c10da537f4f257
Kotolegokot <gkotolegokot@gmail.com>
2016-12-13 00:20:13 +0300

    Add unless to the macro example

--- examples/macro.lisp
+++ examples/macro.lisp
@@ -1,7 +1,11 @@
 (program
   (defvar when (macro (cond &rest body)
                       (backquote (if (interpolate cond) (seq (unfold body))))))
+  (defvar unless (macro (cond &rest body)
+                        (backquote (if (interpolate cond) (interpolate (list)) (seq (unfold body))))))
 
   (when True (print-ln "MEOW") (print-ln "MEOW2"))
-  ; becomes (if Tru
+  ; becomes (if True (seq (print-ln "MEOW") (print-ln "MEOW2")))
+  (unless False (print-ln "MEOW3") (print-ln "MEOW4"))
+  ; becomes (if False (list) (seq (print-ln "MEOW3") (print-ln "MEOW4")))
 )
