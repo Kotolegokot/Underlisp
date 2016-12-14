a09f2baf288d130bf8ff34dc5051daacd1fbb788
Kotolegokot <gkotolegokot@gmail.com>
2016-12-14 22:04:12 +0300

    Add apply => prelude.lisp

--- stdlib/prelude.lisp
+++ stdlib/prelude.lisp
@@ -1,23 +1,23 @@
+; (defmacro macro-name args body)
 (defvar defmacro (macro (name args &rest body)
   `(defvar ~name (macro ~args @body))))
 
+; (when true-condition body)
 (defmacro when (cond &rest body)
   `(if ~cond (seq @body)))
 
+; (unless false-condition body)
 (defmacro unless (cond &rest body)
   `(when (not ~cond) @body))
 
+; (define function-name args body)
 (defmacro define (name args &rest body)
   `(defvar ~name (lambda ~args @body)))
 
+; (load-module module-name)
 (defmacro load-module (filename)
   `(load-context (context-from-file ~filename)))
 
-; (define flip' (f x y)
-;   (bind f y x))
-; 
-; (define flip (f)
-;   (bind (flip' f)))
-; 
-; (defvar ++ (bind + 1))
-; (defvar -- (bind (flip -) 1))
+; (apply function list)
+(defmacro apply (f xs)
+  `(~f @xs))
