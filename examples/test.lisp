cea4bb6c126cef94ba4fd11aa5b32a2c8fd17bf5
Kotolegokot <gkotolegokot@gmail.com>
2016-12-15 11:59:30 +0300

    Add [...] -> (bind ...) sugar

--- examples/test.lisp
+++ examples/test.lisp
@@ -1,5 +1,1 @@
-; (define flip (f)
-;   (lambda (x y) (f y x)))
-; 
-; (print-ln (-  1 2))
-; (print-ln ((flip -) 1 2))
+(print-ln ([+] 1 5))
--- src/Lexer.hs
+++ src/Lexer.hs
@@ -1,45 +1,57 @@
 module Lexer (Lexeme (..), tokenize) where
 
+import qualified Data.Map as Map
+import Data.Map (Map)
 import Data.Char (isSpace)
 import SExpr
 
--- | a lexeme is either a paren or a single atom
-data Lexeme = LeftParen | RightParen | Atom SExpr | Sugar String
-    deriving (Eq, Show)
+data Lexeme = Open Char | Closed Char | Atom SExpr | Sugar String
+  deriving (Eq, Show)
 
 data State = None | Comment | String | OtherAtom
+  deriving (Eq, Show)
 
 -- | takes a string and splits it into lexems
 tokenize :: String -> [Lexeme]
-tokenize sequence = helper [] None sequence
-    where helper lexemes None xs@(x:rest)
-            | x == '('  = helper (LeftParen : lexemes) None rest
-            | x == ')'  = helper (RightParen : lexemes) None rest
-            | x == '\'' = helper (Sugar "quote" : lexemes) None rest
-            | x == '`'  = helper (Sugar "backquote" : lexemes) None rest
-            | x == '~'  = helper (Sugar "interpolate" : lexemes) None rest
-            | x == '@'  = helper (Sugar "unfold" : lexemes) None rest
-            | isSpace x = helper lexemes None rest
-            | x == '"'  = helper lexemes String rest
-            | x == ';'  = helper lexemes Comment rest
-            | otherwise = helper lexemes OtherAtom xs
+tokenize sequence = tokenize' [] None sequence
+    where tokenize' lexemes None xs@(x:rest)
+            | elem x "(["  = tokenize' (Open x                      : lexemes) None rest
+            | elem x ")]"  = tokenize' (Closed (matching_bracket x) : lexemes) None rest
+            | x == '\''    = tokenize' (Sugar "quote"               : lexemes) None rest
+            | x == '`'     = tokenize' (Sugar "backquote"           : lexemes) None rest
+            | x == '~'     = tokenize' (Sugar "interpolate"         : lexemes) None rest
+            | x == '@'     = tokenize' (Sugar "unfold"              : lexemes) None rest
+            | isSpace x    = tokenize' lexemes None rest
+            | x == '"'     = tokenize' lexemes String rest
+            | x == ';'     = tokenize' lexemes Comment rest
+            | otherwise    = tokenize' lexemes OtherAtom xs
 
-          helper lexemes Comment (x:xs) = case x of
-                                            '\n' -> helper lexemes None xs
-                                            _    -> helper lexemes Comment xs
+          tokenize' lexemes Comment (x:xs) = case x of
+                                            '\n' -> tokenize' lexemes None xs
+                                            _    -> tokenize' lexemes Comment xs
 
-          helper lexemes String sequence = parse_string [] sequence
+          tokenize' lexemes String sequence = parse_string [] sequence
               where parse_string string xs@(x:rest)
-                      | x == '"'  = helper (Atom (SString string) : lexemes) None rest
+                      | x == '"'  = tokenize' (Atom (SString string) : lexemes) None rest
                       | otherwise = parse_string (string ++ [x]) rest
 
                     parse_string string [] = error "unexpected EOF in the middle of a string"
 
-          helper lexemes OtherAtom sequence = parse_atom [] sequence
+          tokenize' lexemes OtherAtom sequence = parse_atom [] sequence
               where parse_atom atom xs@(x:rest)
-                      | isSpace x || elem x "()" = helper (Atom (str2atom atom) : lexemes) None xs
-                      | otherwise                = parse_atom (atom ++ [x]) rest
+                      | isSpace x || elem x "[]()" = tokenize' (Atom (str2atom atom) : lexemes) None xs
+                      | otherwise                  = parse_atom (atom ++ [x]) rest
 
-                    parse_atom atom [] = helper (Atom (str2atom atom) : lexemes) None []
+                    parse_atom atom [] = tokenize' (Atom (str2atom atom) : lexemes) None []
 
-          helper lexemes _ [] = reverse lexemes
+          tokenize' lexemes _ [] = reverse lexemes
+
+matching_bracket :: Char -> Char
+matching_bracket x = case x of
+                       '(' -> ')'
+                       ')' -> '('
+                       '[' -> ']'
+                       ']' -> '['
+                       '{' -> '}'
+                       '}' -> '{'
+                       _   -> undefined
