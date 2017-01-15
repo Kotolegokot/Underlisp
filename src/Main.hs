8b86e0356390534b1ef9f362f0d7b1bc13e4869f
Kotolegokot <gkotolegokot@gmail.com>
2017-01-15 17:02:02 +0300

    Add --no-prelude option

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,44 @@
 module Main where
 
+import System.Console.GetOpt
 import System.Environment
+import Data.Maybe (fromMaybe)
 import Interpreter
 import Fail
+
+data Options = Options
+  { optPrelude :: Bool
+  , optHelp    :: Bool
+  } deriving Show
+
+defaultOptions = Options { optPrelude = True
+                         , optHelp    = False }
+
+options :: [OptDescr (Options -> Options)]
+options =
+  [ Option [] ["no-prelude"]
+    (NoArg (\opts -> opts { optPrelude = False }))
+    "Skip loading 'stdlib/prelude.unlisp'",
+    Option ['h'] ["help"]
+    (NoArg (\opts -> opts { optHelp = True } ))
+    "Show help"
+  ]
+
+parseOpts :: [String] -> IO (Options, [String])
+parseOpts argv =
+  case getOpt Permute options argv of
+    (o, n, [])   -> return (foldl (flip id) defaultOptions o, n)
+    (_, _, errs) -> ioError (userError $ concat errs ++ usageInfo header options)
+
+header :: String
+header = "Usage: underlisp [OPTION...] [FILE]"
 
 main :: IO ()
 main = do
   args <- getArgs
-  case args of
-    ["--help"]      -> do
-      exec <- getExecutablePath
-      putStrLn $ "usage: '" ++ exec ++ "' <filename>"
-    (filename:args) -> interpreteProgram True filename args
-    []              -> repl True
+  (opts, restArgs) <- parseOpts args
+  if (optHelp opts)
+    then putStrLn $ usageInfo header options
+    else case restArgs of
+           (filename:args) -> interpreteProgram (optPrelude opts) filename args
+           []              -> repl (optPrelude opts)
